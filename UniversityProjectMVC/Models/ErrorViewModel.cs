06200b0af51857ab2a4fdca15b6aeba41a342f56
1br0h1m <157977082+1br0h1m@users.noreply.github.com>
2025-04-10 20:48:35 +0400

    Initial commit - created MVC project

---
+++ UniversityProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniversityProjectMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
